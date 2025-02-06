9d4688ac0dce20125996276b48fea4892ef0c5c6
Juan David Tirado <tiradodj@uninorte.edu.co>
2025-02-06 09:36:38 -0500

    Topbar design

--- src/components/assets/Topbar.jsx
+++ src/components/assets/Topbar.jsx
@@ -1,7 +1,17 @@
 import "./Topbar.css"
-
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 export default function Topbar() {
     return(
-    <div className="topbar-container"></div>
+    <div className="topbar-container">
+        <div className = "return"><ArrowBackIcon/></div>
+        <div className ="page-name">CNSocial</div>
+        <div className = "right-topbar">
+            <div className = "profile"><AccountBoxIcon/></div>
+            <div className="worldmap"><TravelExploreIcon/></div>
+        </div>
+
+    </div>
     )
 }
